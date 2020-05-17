f6e8b21b3e02e7c2a7ddf31b4a1686674fecee68
Cemu0 <quocanhhai@gmail.com>
2020-05-17 15:18:42 -0600

    update

---
+++ headerRemove.js
@@ -0,0 +1,11 @@
+var elements = document.getElementsByClassName('paper-header');
+
+for (var i = 1; i < elements.length; i++) {
+    elements[i].innerHTML = "";
+}
+
+var elements = document.getElementsByClassName('paper-watermark');
+
+for (var i = 0; i < elements.length; i++) {
+    elements[i].innerHTML = "";
+}
